e14eb7389892c2809aca227962b1595a66a34079
m1ttt <68821845+m1ttt@users.noreply.github.com>
2022-10-08 18:10:24 -0500

    ahora si

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 /* DEFINICIÓN DE PINES */
 
 /* BUS I2C 1 */
-#define SDA_1 21
-#define SCL_1 22
+#define SDA_1 21 // ROJO
+#define SCL_1 22 // NEGRO
 
 /* BUS I2C 2 */
-#define SDA_2 33
-#define SCL_2 32
+#define SDA_2 33 // ROJO 
+#define SCL_2 32 // NEGRO
 
 /* LED DE PLACA */
 #define LED_PLACA 2
@@ -18,36 +18,30 @@
 #include <Adafruit_MPU6050.h> // SSENSOR MPU6050
 #include <Adafruit_Sensor.h>  // LIBRERIA USADA PARA EL SENSOR ADAFRUIT CON TODOS LOS METODOS
 #include <Wire.h>             // LIBRERIA PARA I2C
-#include "libs/wifisetup.h"        // INICIALIZACIÓN DE LA CONEXIÓN WIFI
-#include <Servo.h>            // LIBRERIA PARA EL SERVO (OPCIONAL)
-#include <ArduinoJson.h>      // JSON
-#include <SD.h> 
-#include <SPI.h> 
 
-/* DECLARACION DE VARIABLES */
-const char *config_file = "/config/diccionario.txt";
+// /* DECLARACION DE VARIABLES */
+// const char *config_file = "/config/diccionario.txt";
 
-struct Ori {
-  String orientiacion;
-};
+// struct Ori {
+//   String orientiacion;
+// };
 
-/* INICIALIZACION DE VARIABLES */
-TaskHandle_t tarea1;
-TaskHandle_t tarea2; // VARIABLES PARA EL USO DE NUCLEOS
-TaskHandle_t tarea3;
 
 /* SENSOR MPU6050 */
 Adafruit_MPU6050 sensor;  // MPU6050 (DEDO1)
 Adafruit_MPU6050 sensor2; // MPU6060 (DED02)
 Adafruit_MPU6050 sensor3; // MPU6050 (DED03)
+Adafruit_MPU6050 sensor4; // MPU6060 DED04
+Adafruit_MPU6050 sensor5; 
+Adafruit_MPU6050 sensor6;
 
 
 /* BUSES I2C */
 TwoWire I2Cuno = TwoWire(0); // Primer bus I2C
 TwoWire I2Cdos = TwoWire(1); // Segundo bus I2C
 
-/* VARIABLES PARA LA ESTRUCTUA */ 
-Ori orientacion;
+// /* VARIABLES PARA LA ESTRUCTUA */ 
+// Ori orientacion;
 
 
 
@@ -57,26 +51,37 @@ Ori orientacion;
 // }
 
 /* INICIALIZACION DE FUNCIONES */
-/* CARGAR ORIENTACIONES */
-void cargarOrientacion(const char *config_file, Ori &orientacion){
-  File archivo = SD.open(config_file);
-  
-}
+// /* CARGAR ORIENTACIONES */
+// void cargarOrientacion(const char *config_file, Ori &orientacion){
+//   File archivo = SD.open(config_file);
+//   StaticJsonDocument<200> json;
+//   StaticJsonBuffer<200> json(1024);
+// }
 
-
+boolean vsensor1, vsensor2, vsensor3, vsensor4;
 
 /* INICIALIZACIÓN DE SENSORES */
 void incializarSensores()
 {
   /* INICIALIZACIÓN DE SENSOR DED01 */
-  sensor.setAccelerometerRange(MPU6050_RANGE_2_G); // RANGO DE ACELEROMETRO
+  sensor.setAccelerometerRange(MPU6050_RANGE_16_G); // RANGO DE ACELEROMETRO
   sensor.setGyroRange(MPU6050_RANGE_500_DEG); // RANGO DEL GIROSCOPIO
   sensor.setFilterBandwidth(MPU6050_BAND_5_HZ); 
 
   /* INICIALIZACIÓN DE SENSOR DED02 */
-  sensor2.setAccelerometerRange(MPU6050_RANGE_2_G);
+  sensor2.setAccelerometerRange(MPU6050_RANGE_16_G);
   sensor2.setGyroRange(MPU6050_RANGE_500_DEG);
   sensor2.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+    /* INICIALIZACIÓN DE SENSOR DED02 */
+  sensor3.setAccelerometerRange(MPU6050_RANGE_16_G);
+  sensor3.setGyroRange(MPU6050_RANGE_500_DEG);
+  sensor3.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    /* INICIALIZACIÓN DE SENSOR DED02 */
+
+  sensor4.setAccelerometerRange(MPU6050_RANGE_16_G);
+  sensor4.setGyroRange(MPU6050_RANGE_500_DEG);
+  sensor4.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
 
 /* FUNCION QUE OBTIENE LOS VALORES DEL SENSOR (DED01) */
@@ -86,26 +91,15 @@ void obtenerSensor1()
   sensors_event_t a, g, temp;
   sensor.getEvent(&a, &g, &temp);
   float coord_y = a.acceleration.y;
-Serial.println("");
-  Serial.println("[1]");
-    Serial.println("------------------------------");
-  Serial.print("Acceleration X: ");
+ Serial.print("INICIO");
   Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
   Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
   Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
   Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
   Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
   Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-    Serial.println("------------------------------");
-  
+  Serial.println("FIN");
+
 }
 
 /* FUNCION QUE OBTIENE LOS VALORES DEL SENSOR (DED02) */
@@ -115,26 +109,14 @@ void obtenerSensor2()
   sensors_event_t a, g, temp;
   sensor2.getEvent(&a, &g, &temp);
   float coord_y = a.acceleration.y;
-
-  /* IMPRIME LOS VALORES DE DED02 */ 
-   Serial.println("[2]");
-   Serial.println("------------------------------");
-  Serial.print("Acceleration X: ");
+ Serial.print("INICIO");
   Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
   Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
   Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
   Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
   Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
   Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-    Serial.println("------------------------------");
+  Serial.println("FIN");
 }
 
 
@@ -145,26 +127,30 @@ void obtenerSensor3()
   sensors_event_t a, g, temp;
   sensor3.getEvent(&a, &g, &temp);
   float coord_y = a.acceleration.y;
-
-  /* IMPRIME LOS VALORES DE DED02 */ 
-   Serial.println("[3]");
-   Serial.println("------------------------------");
-  Serial.print("Acceleration X: ");
+  Serial.print("INICIO");
   Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
   Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
   Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
   Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
   Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
   Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-    Serial.println("------------------------------");
+  Serial.println("FIN");
+}
+
+void obtenerSensor4()
+{
+  /* OBTIENE LOS VALORES DEL SENSOR DED02*/
+  sensors_event_t a, g, temp;
+  sensor4.getEvent(&a, &g, &temp);
+  float coord_y = a.acceleration.y;
+  Serial.print("INICIO");
+  Serial.print(a.acceleration.x);
+  Serial.print(a.acceleration.y);
+  Serial.print(a.acceleration.z);
+  Serial.print(g.gyro.x);
+  Serial.print(g.gyro.y);
+  Serial.print(g.gyro.z);
+  Serial.println("FIN");
 }
 
 
@@ -180,9 +166,17 @@ void setup()
     delay(10);
   }
   Serial.println("Prueba de MPU6050 (4)");
-  if (!sensor.begin(0x68, &I2Cuno) || !sensor2.begin(0x69, &I2Cuno) || !sensor3.begin(0x68, &I2Cdos)) // INICIO DE SENSORES.
+  vsensor1 = sensor.begin(0x68, &I2Cuno);
+  vsensor2 = sensor2.begin(0x69, &I2Cuno);
+  vsensor3 = sensor3.begin(0x69, &I2Cdos);
+  vsensor4 = sensor4.begin(0x68, &I2Cdos);
+  if (!vsensor1 || !vsensor2 || !vsensor3 || !vsensor4) // INICIO DE SENSORES.
   {
     Serial.println("No se pudo conectar con alguno de los sensores");
+    Serial.println(vsensor1);
+    Serial.println(vsensor2);
+    Serial.println(vsensor3);
+    Serial.println(vsensor4);
     for (;;) // BUCLE DE LED
     {
       digitalWrite(LED_PLACA, HIGH);
@@ -208,5 +202,6 @@ void loop()
   obtenerSensor1(); // OBTIENE VALORES DEL SENSOR 1 (DED01)
   obtenerSensor2(); // OBTIENE VALORES DEL SENSOR 2 (DED02)
   obtenerSensor3();
-  delay(2000);
+  obtenerSensor4();
+  delay(500);
 }
